f737c8970e558a2543881382055b7301d21e2d2a
mepo1496 <md.rafi.uzzaman@uni-weimar.de>
2026-05-17 22:59:19 +0200

    Add Main class to launch login window

---
+++ src/main/java/de/Main.java
@@ -0,0 +1,12 @@
+
+package de.buw.se;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.show();
+
+    }
+}
